b2259e4625d909a9c19fb6bba71e3175f44db920
Viktoriia Ivanova <iv.vik.19.07@gmail.com>
2020-11-28 00:32:06 +0200

    practical task

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -2,9 +2,9 @@ SELECT p.first_name, p.last_name, COUNT(t.order_id) as total_orders, SUM(t.quant
 FROM person p LEFT OUTER JOIN 
                              (SELECT o.id as order_id, o.person_id, SUM(oi.quantity) as quantity, (SUM(oi.quantity*i.price-oi.discount)) AS total_money
                               FROM order_ o 
-				   			  JOIN order_item oi ON o.id = oi.order_id
-			       			  JOIN item i ON oi.item_id = i.id  
-							  GROUP BY o.id)t 
+			      JOIN order_item oi ON o.id = oi.order_id
+			      JOIN item i ON oi.item_id = i.id  
+			      GROUP BY o.id)t 
                               ON p.id = t.person_id
 GROUP BY p.first_name
 ORDER BY p.id
