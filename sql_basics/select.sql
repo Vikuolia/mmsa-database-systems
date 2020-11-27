7132a3b74121aad183c91ebba64e7ef648b9a3ff
Viktoriia Ivanova <iv.vik.19.07@gmail.com>
2020-11-28 00:40:55 +0200

    update

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -1,6 +1,6 @@
 SELECT p.first_name, p.last_name, COUNT(t.order_id) as total_orders, SUM(t.quantity) as total_items_bought, SUM(t.total_money) as total_money_spent
 FROM person p LEFT OUTER JOIN 
-                             (SELECT o.id as order_id, o.person_id, SUM(oi.quantity) as quantity, (SUM(oi.quantity*i.price-oi.discount)) AS total_money
+                             (SELECT o.id as order_id, o.person_id, SUM(oi.quantity) as quantity, (SUM(oi.quantity*(i.price-oi.discount))) AS total_money
                               FROM order_ o 
 			      JOIN order_item oi ON o.id = oi.order_id
 			      JOIN item i ON oi.item_id = i.id  
