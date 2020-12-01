ff4a8ce800f25cc24bdfc12a5a5750dadc67b9fb
Viktoriia Ivanova <iv.vik.19.07@gmail.com>
2020-12-01 21:10:20 +0200

    update

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -1,10 +1,8 @@
-SELECT p.first_name, p.last_name, COUNT(t.order_id) as total_orders, SUM(t.quantity) as total_items_bought, SUM(t.total_money) as total_money_spent
-FROM person p LEFT OUTER JOIN 
-                             (SELECT o.id as order_id, o.person_id, SUM(oi.quantity) as quantity, (SUM(oi.quantity*(i.price-oi.discount))) AS total_money
-                              FROM order_ o 
-			      JOIN order_item oi ON o.id = oi.order_id
-			      JOIN item i ON oi.item_id = i.id  
-			      GROUP BY o.id)t 
-                              ON p.id = t.person_id
-GROUP BY p.first_name
-ORDER BY p.id
+SELECT p.first_name, p.last_name, COUNT(DISTINCT(o.id)) as order_id, SUM(oi.quantity) as quantity, SUM((oi.quantity*(i.price-oi.discount))) AS total_money
+FROM person p LEFT JOIN         
+			 (order_ o 
+             JOIN order_item oi ON o.id = oi.order_id
+             JOIN item i
+             ON oi.item_id = i.id  
+             ) ON p.id = o.person_id
+GROUP BY p.id
